5572140b6666a532d91bbb2004c8029e495afa0b
marcosecchi <loziosecchi@gmail.com>
2022-02-28 19:40:25 +0100

    Refactor AbstractPlayerController.

--- Assets/Scripts/Player/AbstractPlayerController.cs
+++ Assets/Scripts/Player/AbstractPlayerController.cs
@@ -5,7 +5,75 @@ using UnityEngine;
 
 namespace TheBitCave.MultiplayerRoguelite
 {
+    [RequireComponent(typeof(CharacterController))]
     public abstract class AbstractPlayerController : NetworkBehaviour
     {
+        private CharacterController _characterController;
+        private Animator _animator;
+        private InputActions _inputActions;
+
+        [SerializeField]
+        private float walkSpeed = 3;
+
+        [SerializeField]
+        private float runSpeed = 3;
+
+        [SerializeField]
+        private float rotationSpeed = 3;
+
+        protected virtual void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _animator = GetComponentInChildren<Animator>();
+            var networkAnimator = GetComponent<NetworkAnimator>();
+            if (networkAnimator != null) networkAnimator.animator = _animator;
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            _inputActions = new InputActions();
+            _inputActions.Player.Enable();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            // Player Input
+            if (!isLocalPlayer) return;
+            var input = _inputActions.Player.Move.ReadValue<Vector2>();
+            var isRunning = _inputActions.Player.Run.inProgress;
+            var speed = isRunning ? runSpeed : walkSpeed;
+            
+            // Character Movement
+            var move = input.y * speed * transform.forward;
+            _characterController.Move(move);
+
+            var rotation = input.x * rotationSpeed * transform.up;
+            transform.Rotate(rotation);
+            
+            // Animator update
+            if (_animator == null) return;
+            _animator.SetFloat(C.ANIMATOR_PARAMETER_SPEED, _characterController.velocity.magnitude);
+        }
+
+        /*
+        private void OnEnable()
+        {
+            if (!isLocalPlayer) return;
+//            _inputActions.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            if (!isLocalPlayer) return;
+            _inputActions.Player.Disable();
+        }
+        */
+
     }
 }
--- Assets/Scripts/Prototyping/ProtoPlayerController.cs
+++ Assets/Scripts/Prototyping/ProtoPlayerController.cs
@@ -6,21 +6,8 @@ using UnityEngine;
 
 namespace TheBitCave.MultiplayerRoguelite.Prototype
 {
-    [RequireComponent(typeof(CharacterController))]
     public class ProtoPlayerController : AbstractPlayerController
     {
-        private CharacterController _characterController;
-        private Animator _animator;
-        private InputActions _inputActions;
-
-        [SerializeField]
-        private float walkSpeed = 3;
-
-        [SerializeField]
-        private float runSpeed = 3;
-
-        [SerializeField]
-        private float rotationSpeed = 3;
 
         [SerializeField]
         private TextMeshProUGUI pointsLabel;
@@ -30,24 +17,10 @@ namespace TheBitCave.MultiplayerRoguelite.Prototype
 
         [SyncVar(hook = nameof(OnPointsChange))]
         private int _points = -1;
-
-        private void Awake()
-        {
-            _characterController = GetComponent<CharacterController>();
-            _animator = GetComponentInChildren<Animator>();
-            var networkAnimator = GetComponent<NetworkAnimator>();
-            if (networkAnimator != null) networkAnimator.animator = _animator;
-        }
-
-        public override void OnStartLocalPlayer()
-        {
-            base.OnStartLocalPlayer();
-            _inputActions = new InputActions();
-            _inputActions.Player.Enable();
-        }
-
+        
         public override void OnStartServer()
         {
+            base.OnStartServer();
             _skinColor = Random.ColorHSV();
             _points = 0;
         }
@@ -69,40 +42,5 @@ namespace TheBitCave.MultiplayerRoguelite.Prototype
         {
             pointsLabel.text = newValue.ToString();
         }
-        
-        private void FixedUpdate()
-        {
-            // Player Input
-            if (!isLocalPlayer) return;
-            var input = _inputActions.Player.Move.ReadValue<Vector2>();
-            var isRunning = _inputActions.Player.Run.inProgress;
-            var speed = isRunning ? runSpeed : walkSpeed;
-            
-            // Character Movement
-            var move = input.y * speed * transform.forward;
-            _characterController.Move(move);
-
-            var rotation = input.x * rotationSpeed * transform.up;
-            transform.Rotate(rotation);
-            
-            // Animator update
-            if (_animator == null) return;
-            _animator.SetFloat(C.ANIMATOR_PARAMETER_SPEED, _characterController.velocity.magnitude);
-        }
-
-        /*
-        private void OnEnable()
-        {
-            if (!isLocalPlayer) return;
-//            _inputActions.Player.Enable();
-        }
-
-        private void OnDisable()
-        {
-            if (!isLocalPlayer) return;
-            _inputActions.Player.Disable();
-        }
-        */
     }
-    
 }
