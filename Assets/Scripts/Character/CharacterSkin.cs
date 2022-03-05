72e80906ce34eee1bc2964f095ec770ffc648ac0
marcosecchi <loziosecchi@gmail.com>
2022-03-05 16:56:49 +0100

    Add basic loading skin support.

--- Assets/Scripts/Character/CharacterSkin.cs
+++ Assets/Scripts/Character/CharacterSkin.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace TheBitCave.MultiplayerRoguelite
 {
@@ -13,9 +14,37 @@ namespace TheBitCave.MultiplayerRoguelite
         [SerializeField] protected Transform armLeftSlot;
         [SerializeField] protected Transform armRightSlot;
 
-        public override void OnStartClient()
+        public override void OnStartServer()
         {
-            base.OnStartClient();
+            base.OnStartServer();
+
+            // TODO: Remove from Sync operation
+            // TODO: Add skin randomization
+            
+            var op = Addressables.LoadAssetAsync<GameObject>("body");
+            var prefab = op.WaitForCompletion();
+            AddBodyPart(prefab, bodySlot);
+
+            op = Addressables.LoadAssetAsync<GameObject>("armLeft");
+            prefab = op.WaitForCompletion();
+            AddBodyPart(prefab, armLeftSlot);
+            
+            op = Addressables.LoadAssetAsync<GameObject>("armRight");
+            prefab = op.WaitForCompletion();
+            AddBodyPart(prefab, armRightSlot);
+            
+            op = Addressables.LoadAssetAsync<GameObject>("head");
+            prefab = op.WaitForCompletion();
+            AddBodyPart(prefab, headSlot);
+            
+            Addressables.Release(op);
+        }
+
+        protected virtual void AddBodyPart(GameObject prefab, Transform parent)
+        {
+            var go = Instantiate(prefab, parent);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localRotation = Quaternion.identity;
         }
     }
 }
